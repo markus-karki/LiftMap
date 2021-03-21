4720c7ea88883207fabc4572abff6403064747e3
markus-karki <markus-karki@users.noreply.github.com>
2021-03-21 15:30:32 +0200

    EOD_20210821

---
+++ CalibrateModel.py
@@ -0,0 +1,129 @@
+# Open lift database
+
+import rasterio
+import rasterio.crs
+from rasterio.transform import Affine
+
+import json
+from sklearn.linear_model import LogisticRegression
+
+from sqlite3 import connect
+
+
+from pandas import DataFrame, read_sql_query
+from numpy import zeros, unique, save, load, sort, meshgrid, linspace, reshape, flipud, ones, minimum, maximum
+
+from pyproj import Proj, Transformer 
+
+#import plotly.express as px
+
+import pickle
+        
+class Geodata:
+    def __init__(self, geodatasets):
+        self.utm_transformer = Transformer.from_crs(32635, 3035)
+        self.webMerc_transformer = Transformer.from_crs(3857, 3035)
+        self.dataset = rasterio.open(geodatasets)
+        self.data = self.dataset.read(1)
+    
+    def getFeatureVector(self, lon, lat, coord_type='utm'):
+        n = lon.shape[0]
+        X = zeros((n, 256))
+        a = 5
+        for i in range(n):
+            if coord_type == 'webMercator':
+                [y, x] = self.webMerc_transformer.transform(lon[i], lat[i])
+            else:
+                [y, x] = self.utm_transformer.transform(lon[i], lat[i])
+            [x_ind, y_ind] = self.dataset.index(x,y)
+            [values, counts] = unique(self.data[(x_ind-a):(x_ind+a), (y_ind-a):(y_ind+a)], return_counts=True)
+            X[i, values] = counts / ((2*a)**2)
+        return X
+
+class Dataset:
+    def __init__(self, databaseName):
+        conn = connect(databaseName + '.db')
+        self.data = read_sql_query('SELECT * FROM Estimates', conn)
+
+    def getTrainingData(self, geodata):
+        y = self.data['isLift'].round().to_numpy()
+        X = geodata.getFeatureVector(self.data['x_lift'].to_numpy(), self.data['y_lift'].to_numpy())
+        return X, y
+
+def colorMap(Z):
+    midpoint = 0.3 * 255
+    out = zeros((Z.shape[0],Z.shape[1], 3))
+    one = ones((Z.shape[0],Z.shape[1]))*255
+    out[:,:,0] = minimum(Z/midpoint*255, one)
+    out[:,:,1] = maximum(255 - abs(Z-midpoint), one*0)
+    out[:,:,2] = minimum(-255/(255-midpoint)*Z+(255*255)/(255-midpoint), one)
+    return out.astype('uint8')
+
+def trainModel(database_name, geodatasets):
+    # create geodata
+    geodata = Geodata(geodatasets)
+
+    # open database
+    dataset = Dataset(database_name)
+    [X, y] = dataset.getTrainingData(geodata)
+
+    # Train model
+    clf = LogisticRegression(random_state=0).fit(X, y)
+
+    # Print score
+    #p=clf.predict_proba(X)
+    #p=sort(p[:,1])
+    #fig = px.line(p)
+    #fig.show()
+
+    # Save parameters
+    return clf
+
+def predictLift(geodatasets, clf):
+    # create geodata
+    geodata = Geodata(geodatasets)    
+
+    # Calculate georaster
+    x_min = 2500000
+    x_n = 300
+    x_max = 2800000
+
+    y_min = 8400000
+    y_n = 300
+    y_max = 8700000
+    
+
+    [xx, yy] = meshgrid(linspace(x_min, x_max, x_n),linspace(y_min, y_max, y_n))
+
+    X = geodata.getFeatureVector(reshape(xx, (-1,)), reshape(yy,(-1,)), 'webMercator')
+
+    #Open model
+    zz = clf.predict_proba(X)
+
+    Z = flipud(reshape(zz[:,1]*255, (y_n, x_n))).astype('uint8')
+    Z = colorMap(Z)
+    # Save results
+    res = (x_max - x_min) / x_n
+    transform = Affine.translation(x_min - res / 2, y_max + res / 2) * Affine.scale(res, - res)
+
+    new_dataset = rasterio.open(
+        './results/new2.tif',
+        'w',
+        driver='GTiff',
+        height=Z.shape[0],
+        width=Z.shape[1],
+        count=3,
+        dtype=Z.dtype,
+        crs=rasterio.crs.CRS({"init": "epsg:3857"}),
+        transform=transform
+        )
+    new_dataset.write(Z[:,:,0], 1)
+    new_dataset.write(Z[:,:,1], 2)
+    new_dataset.write(Z[:,:,2], 3)
+    new_dataset.close()
+
+if __name__ == '__main__':
+    datasets = './data/landcover/DATA/U2018_CLC2018_V2020_20u1.tif'
+    database_name = 'database'
+    clf = trainModel(database_name, datasets)
+    predictLift(datasets, clf)
---
+++ VisualizeData.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Markus Kärki'
+__version__ = "0.0.0"
+
+'''
+A script for creating visualization on lift data in a sqlite database. Needs database name as input
+
+Updates
+    -
+
+Todo
+    - plot coastline
+    - plot turnpoints
+    - plot data
+    - plot 
+
+Known issues
+    -
+'''
+from sqlite3 import connect
+from pandas import DataFrame, read_csv, read_sql_query
+
+import json
+import plotly.express as px
+import plotly.graph_objects as go
+
+from pyproj import Proj
+
+import rasterio
+
+def soil_data_test():
+    dataset = rasterio.open('./data/wrbfu_directory/wrbfu/hdr.adf')
+
+    #dataset = rasterio.open('./data/landcover/DATA/U2018_CLC2018_V2020_20u1.tif')
+
+
+
+def main(databaseName):
+    # Open database
+    conn = connect(databaseName + '.db')
+    cursor = conn.cursor()
+
+
+    # Open turnpoint data
+
+    # Open landcover data
+    #https://land.copernicus.eu/pan-european/corine-land-cover/clc2018
+   # https://gist.github.com/Turbo87/72d73a548dbf953e1f5e
+   #response = requests.get("https://skylines.aero/api/flights/date/2019-05-19")
+    # Open soil data
+    #https://digital-geography.com/free-global-soil-grids-1km-resolution/
+    #https://esdac.jrc.ec.europa.eu/ApplicationAndServices
+    
+    # Get datapoints    
+    #cursor.execute('SELECT * FROM Estimates', ())
+    #df = DataFrame(cursor.fetchall())#, columns=['x_lift', 'y_lift', 'isLift'])
+    df = read_sql_query('SELECT * FROM Estimates', conn)
+    param = json.load(open('projection.json'))   
+    p=Proj(param)
+    lon, lat = p(df['x_lift'].tolist(), df['y_lift'].tolist(), inverse=True)
+    df['Latitude']=lat 
+    df['Longitude'] = lon
+
+    fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", color='isLift', hover_data=['Time','z','w_x','w_y','w_z'])
+    fig.update_layout(mapbox_style="carto-positron")
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+    fig.show()
+
+def map_test():
+
+    df_turnpoints = read_csv('finland8.cup', encoding='latin1' )
+    
+    df_turnpoints['Latitude'] = (df_turnpoints['Latitude'].str[0:2].astype(float) 
+        + df_turnpoints['Latitude'].str[2:4].astype(float)/100/.6  
+        + df_turnpoints['Latitude'].str[5:8].astype(float)/100000/.6)
+    
+    df_turnpoints['Longitude'] = (df_turnpoints['Longitude'].str[0:3].astype(float) 
+        + df_turnpoints['Longitude'].str[3:5].astype(float)/100/.6 
+        + df_turnpoints['Longitude'].str[6:9].astype(float)/100000/.6)
+    
+    #countries = json.load(open("./countries/CNTR_BN_60M_2020_3035_COASTL.geojson"))
+    
+    fig = px.scatter_mapbox(df_turnpoints, lat="Latitude", lon="Longitude", hover_name="Title", hover_data=["Description"])
+    fig.update_layout(mapbox_style="carto-positron")
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+    fig.show()
+
+if __name__ == '__main__':
+    databaseName = "database"
+    #main(databaseName)
+    #map_test()
+    soil_data_test()
